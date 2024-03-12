9735eda12d70d48857857163537335cf338764e7
Lee Stables <lee.stables@reachstudios.co.uk>
2024-03-12 19:46:46 +0000

    Initial setup

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+
+class ApiController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        // Keys
+        $apiKey = "PCW45-12345-12345-1234X";
+        $countryCode = "UK";
+        $searchTerm = "NR1 1NE";
+
+        // Prepare URL
+        $requestUrl = "https://ws.postcoder.com/pcw/$apiKey/address/$countryCode/" . urlencode($searchTerm);
+
+        $response = Http::withoutVerifying()->get($requestUrl);
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $addresses = $response->json();
+
+            if (count($addresses) > 0) {
+                foreach ($addresses as $address) {
+                    echo($address['summaryline'] . "<br>");
+                }
+            } else {
+                echo "No results";
+            }
+        } else {
+
+            if ($response->clientError()) {
+                echo "Client Error";
+            } elseif ($response->serverError()) {
+                echo "Server Error";
+            } else {
+                echo "Request error";
+            }
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::post('/tokens/create', function (Request $request) {
+//     $token = $request->user()->createToken($request->token_name);
+
+//     return ['token' => $token->plainTextToken];
+// });
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::get('/lookup', ApiController::class)->middleware('auth:sanctum');
